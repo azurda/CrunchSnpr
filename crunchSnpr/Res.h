d00393ebf7688e50af0e0cc31ef3ebd6a981def1
ineedblood <aztec_eu@live.com>
2015-03-30 14:11:18 +0200

    Needed dependencies
    New objects values and definitions for later use

---
+++ crunchSnpr/Res.h
@@ -0,0 +1,19 @@
+#define IDD_CRUNCHSNPR_DIALOG	102
+#define IDI_SMALL				108
+#define IDR_MAINFRAME			128
+#define IDI_ICON1				132
+#define IDI_CRUNCHSNPR			132
+#define IDI_STATIC				-1
+
+// default values for new objects
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC				1
+#define _APS_NEXT_RESOURCE_VALUE 134
+#define _APS_NEXT_COMMAND_VALUE  32771
+#define _APS_NEXT_CONTROL_VALUE	 1000
+#define _APS_NEXT_SYMED_VALUE	 110
+#endif
+#endif
+
